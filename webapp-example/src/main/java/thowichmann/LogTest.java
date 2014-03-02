d65e04ef645f808251e1ddb0b87a8447f27d59d0
Thomas Wichmann <beren.wichmann@web.de>
2014-03-03 00:33:56 +0100

    cleanup

--- webapp-example/src/main/java/thowichmann/LogTest.java
+++ webapp-example/src/main/java/thowichmann/LogTest.java
@@ -3,6 +3,7 @@ package thowichmann;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +21,9 @@ public class LogTest extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		LoggerFactory.getLogger(getClass()).error("This is a test error log message.");
+		ServletOutputStream out = response.getOutputStream();
+		out.println("Message logged");
+		out.close();
 	}
 
 }
