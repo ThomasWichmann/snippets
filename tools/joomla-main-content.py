9ee6db377b77dc43db74664f63216991275dc3b8
Thomas Wichmann <beren.wichmann@web.de>
2016-05-12 22:33:31 +0200

    in progress: Joomla content export script

---
+++ tools/joomla-main-content.py
@@ -0,0 +1,51 @@
+Not yet usable!
+#!/usr/bin/env python3
+#
+# Converts a Joomla HTML page with standard templates into markdown syntax.
+# Only main content in converted and everything else removed.
+#
+# This script might be used for other HTML page conversion
+#
+
+import getopt
+import os
+import re
+import sys
+
+opts, args = getopt.getopt(sys.argv[1:], "d:n:v:")
+
+isReadOnly = False
+isVerbose = False
+rootDirectory = ""
+
+for opt, arg in opts:
+    if opt == "-d":
+        rootDirectory = arg
+    if opt == "-n":
+        isReadOnly = True
+    if opt == "-v":
+        isVerbose = True
+
+filePattern = ".*.html"
+searchPatternSwitches = re.MULTILINE
+
+if not rootDirectory:
+    raise ValueError("No directory to search for HTML files given.")
+
+print("Converting", rootDirectory, "for files like ", filePattern )
+
+for subdir, dirs, files in os.walk(rootDirectory):
+    for file in files:
+        path = os.path.join(subdir, file)
+        if (re.match(r'^'+filePattern+'$', file)):
+            print("Convert file :", path)
+            input = open(path,'r') 
+            content = input.read()
+            content = re.sub(r'', r'', content, 0, searchPatternSwitches)
+            if not isReadOnly:
+                output = open(path, 'w')
+                output.write(content)
+        else:
+            if isVerbose:
+                print("No matching files found in ", path)
+
