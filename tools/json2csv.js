defdecf2f2a33a348ad8ab404f671b25e2613075
Thomas Wichmann <thomas.wichmann@dbc-gmbh.com>
2016-06-10 19:18:02 +0200

    improved CLI

--- tools/json2csv.js
+++ tools/json2csv.js
@@ -2,7 +2,7 @@
 /*
  * Fully read JSON file, parse included objects and output them as CSV using NodeJS.
  * 
- * Call it like json2csv.js test.json [objectName] [comma separated list of to be converted properties]
+ * Call it like json2csv.js -f test.json [-r objectName] [-p 'comma separated list of to be converted properties']
  * 
  * No escaping of output data is done. The input data is assumed to be an array of objects. The objects should have 
  * all relevant fields in common. The number of columns in the CSV output is the sum over all different object fields.
@@ -13,14 +13,17 @@
 
 'use strict';
 
-var filename = process.argv[2]; // first argument
-var fs = require('fs');
+var filename;
+if(process.argv.indexOf("-f") != -1){
+	filename = process.argv[process.argv.indexOf("-f") + 1];
+}
 
+var fs = require('fs');
 var text = fs.readFileSync(filename).toString();
 
 var list;
-if (process.argv[3]) {
-	var objectName = process.argv[3];
+if(process.argv.indexOf("-r") != -1){
+	var objectName = process.argv[process.argv.indexOf("-r")+1];
 	list = JSON.parse(text)[objectName];
 } else {
 	list = JSON.parse(text);
@@ -28,8 +31,8 @@ if (process.argv[3]) {
 var listLength = list.length;
 
 var properties = {};
-if (process.argv[4]) {	
-	process.argv[4].split(',').reduce(function fct(properties, name){properties[name]=null; return properties}, properties);
+if(process.argv.indexOf("-p") != -1){
+	 process.argv[process.argv.indexOf("-p")+1].split(',').reduce(function fct(properties, name){properties[name]=null; return properties}, properties);
 } else {
 
 	for (var i = 0; i < listLength; i++) {
